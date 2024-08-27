70baa38c17561a69dd20b2d7670bd50f30fe849b
fedtoscano <federicotoscano611@gmail.com>
2024-08-27 23:37:10 +0200

    seedata la categoria

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -15,6 +15,8 @@ class Category extends Model
     use HasFactory;
     use SoftDeletes;
 
+    protected $fillable = ["name", "color"];
+
     public function books(){
         return $this->hasMany(Book::class);
     }
--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,16 +2,34 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class CategorySeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
         //
+        $categories = [
+            ['name' => 'Novel', 'color' => '#FF5733'],
+            ['name' => 'Essay', 'color' => '#33FF57'],
+            ['name' => 'Scientific', 'color' => '#3357FF'],
+            ['name' => 'Poetry', 'color' => '#F4A300'],
+            ['name' => 'Self-Help', 'color' => '#F40077'],
+            ['name' => 'History', 'color' => '#A3F400'],
+            ['name' => 'Philosophy', 'color' => '#F4B800'],
+            ['name' => 'Drama', 'color' => '#B800F4'],
+            ['name' => 'Travel', 'color' => '#F4F400'],
+        ];
+
+        foreach ($categories as $category) {
+            Category::create($category);
+        }
     }
 }
