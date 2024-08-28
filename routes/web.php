3cf6e616fb87ca3fab182cccb05bdfaabac51059
fedtoscano <federicotoscano611@gmail.com>
2024-08-28 17:35:20 +0200

    create le rotte admin/ per ogni risorsa

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
+//CUSTOM CONTROLLERS
+use App\Http\Controllers\Admin\BookController;
+use App\Http\Controllers\Admin\AuthorController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\EditorController;
+use App\Http\Controllers\Admin\LoanController;
+use App\Http\Controllers\Admin\TranslatorController;
+use App\Http\Controllers\Admin\UserController;
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Auth;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,9 +22,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.home');
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware("auth")->name("admin.")->prefix("admin/")->group(function(){
+    Route::resource('books', BookController::class);
+    Route::resource('authors', AuthorController::class);
+    Route::resource('categories', CategoryController::class);
+    Route::resource('editors', EditorController::class);
+    Route::resource('loans', LoanController::class);
+    Route::resource('translators', TranslatorController::class);
+    Route::resource('users', UserController::class);
+});
+
+
